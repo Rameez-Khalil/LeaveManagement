74078b1b9a4fd0ecd23411c4611a34e12db1ab77
rameez <rameezkhalil23@gmail.com>
2024-03-02 05:15:58 -0800

    Updated layout

---
+++ LeaveManagement.Web/Notes/Section3 - Project Setup and Config/01_Control_Flow.cs
@@ -0,0 +1,6 @@
+﻿/*
+ * CONTROL FLOW:  
+ *      Any controller with an index action will be the first view to be viewed.
+ *      If we click on privacy, then it will go into the controller (Home etc.) and then it will go into Privacy and will be returning the view.
+ *      
+ */
