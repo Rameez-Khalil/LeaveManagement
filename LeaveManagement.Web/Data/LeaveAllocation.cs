98e631c15c4eb3d7de137df55cd59edcd563a412
rameez <rameezkhalil23@gmail.com>
2024-03-04 15:44:02 -0800

    Added Database Tables, and Migrations

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+      
+        public string Name { get; set; }
+        
+
+        //EF will automatically know that this is a foreign key relationship.
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public string EmployeeId { get; set; }
+    }
+}
---
+++ LeaveManagement.Web/Data/LeaveType.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaveManagement.Web.Data
+{
+
+    //These props will be translated into SQL tables/cols.
+    public class LeaveType : BaseEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+     
+
+    }
+}
